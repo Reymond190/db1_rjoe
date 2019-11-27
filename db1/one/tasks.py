9323d5087f271046c126b2e01c9f5f9c3ec377be
reymond <josephreymond2011@gmail.com>
2019-11-27 12:47:53 +0530

    commit 3.0097

--- db1/one/tasks.py
+++ db1/one/tasks.py
@@ -22,33 +22,35 @@ from django.utils.crypto import get_random_string
 from celery import shared_task
 from .models import Alert ,api1
 
-@shared_task
-def random():
-    i = Alert()
-    time2 = datetime.datetime.now()
-    i.time = time2
-    i.save()
-
-
-def get_api():
-    time2 = datetime.datetime.now()
-    time1 = time2 + timedelta(seconds=-10)
-    time1 = time1.strftime("%Y-%m-%d %H:%M:%S")
-    time2 = time2.strftime("%Y-%m-%d %H:%M:%S")
-    time1 = str(time1)
-    time2 = str(time2)
-    r1 = requests.get('https://lnt.tracalogic.co/api/ktrack/larsentoubro/' + time1 + '/' + time2,
-                      auth=HTTPBasicAuth('admin', 'admin'))
-    x1 = r1.json()
-    x2 = json.dumps(x1)
-    y1 = json.loads(x2)
-    return y1
-
+# @shared_task
+# def random():
+#     i = Alert()
+#     time2 = datetime.datetime.now()
+#     i.time = time2
+#     i.save()
 
 
 def get_dataframe(y1):
     df1 = json_normalize(y1["assetHistory"])
     return df1
+
+
+def get_api():
+    time2 = datetime.datetime.now()
+    print(time2)
+    time1 = time2 + timedelta(seconds=-10)
+    time1 = time1.strftime("%Y-%m-%d %H:%M:%S")
+    time2 = time2.strftime("%Y-%m-%d %H:%M:%S")
+    time1 = str(time1)
+    time2 = str(time2)
+    print(time2)
+    r1 = requests.get('https://lnt.tracalogic.co/api/ktrack/larsentoubro/' + time1 + '/' + time2,
+                      auth=HTTPBasicAuth('admin', 'admin'))
+    x1 = r1.json()
+    print(x1)
+    x2 = json.dumps(x1)
+    y1 = json.loads(x2)
+    return y1
 
 
 @shared_task
