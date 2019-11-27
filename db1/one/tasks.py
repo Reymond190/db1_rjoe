a6da80e94b6101e30771330d2f151e7058495682
reymond <josephreymond2011@gmail.com>
2019-11-27 12:55:55 +0530

    commit 3.0095

--- db1/one/tasks.py
+++ db1/one/tasks.py
@@ -37,17 +37,14 @@ def get_dataframe(y1):
 
 def get_api():
     time2 = datetime.datetime.now()
-    print(time2)
     time1 = time2 + timedelta(seconds=-10)
     time1 = time1.strftime("%Y-%m-%d %H:%M:%S")
     time2 = time2.strftime("%Y-%m-%d %H:%M:%S")
     time1 = str(time1)
     time2 = str(time2)
-    print(time2)
     r1 = requests.get('https://lnt.tracalogic.co/api/ktrack/larsentoubro/' + time1 + '/' + time2,
                       auth=HTTPBasicAuth('admin', 'admin'))
     x1 = r1.json()
-    print(x1)
     x2 = json.dumps(x1)
     y1 = json.loads(x2)
     return y1
@@ -55,11 +52,8 @@ def get_api():
 
 @shared_task
 def all():
-
-
     r = get_api()
     d = get_dataframe(r)
-
     d1 = d
 
     a1 = api1.objects.get(No="1")
